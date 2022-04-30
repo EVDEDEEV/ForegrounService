c4fb0eac6e72883c7647f669c6c4f64628612ba4
Erkesh <dedeeverkesh@gmail.com>
2022-04-30 03:19:02 +0300

    Filled MyService.kt

--- app/src/main/java/my/project/foregroundservice/MyService.kt
+++ app/src/main/java/my/project/foregroundservice/MyService.kt
@@ -1,13 +1,17 @@
 package my.project.foregroundservice
 
-import android.app.Service
+import android.annotation.SuppressLint
+import android.app.*
 import android.content.Intent
 import android.media.MediaPlayer
+import android.os.Build
 import android.os.IBinder
+import my.project.foregroundservice.Constants.CHANNEL_ID
+import my.project.foregroundservice.Constants.MUSIC_NOTIFICATION_ID
 
-class MyService:Service() {
+class MyService : Service() {
 
-    private lateinit var musicPlayer:MediaPlayer
+    private lateinit var musicPlayer: MediaPlayer
 
     override fun onBind(p0: Intent?): IBinder? {
         return null
@@ -16,10 +20,48 @@ class MyService:Service() {
     override fun onCreate() {
         super.onCreate()
         initMusic()
-        create
+        crateNotificationChannel()
     }
 
-    private fun crateNotificationChannel()
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        showNotification()
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    @SuppressLint("NewApi")
+    private fun showNotification() {
+        val notificationIntent = Intent(this, MainActivity::class.java)
+
+        val pendingIntent = PendingIntent.getActivity(
+            this, 0, notificationIntent, 0
+        )
+
+        val notification = Notification
+            .Builder(this, CHANNEL_ID)
+            .setContentText("Music Player")
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentIntent(pendingIntent)
+            .build()
+
+        startForeground(MUSIC_NOTIFICATION_ID, notification)
+
+    }
+
+    private fun crateNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            val serviceChannel = NotificationChannel(
+                CHANNEL_ID, "My Service Channel",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+
+            val manager = getSystemService(
+                NotificationManager::class.java
+            )
+
+            manager.createNotificationChannel(serviceChannel)
+        }
+    }
 
 
     private fun initMusic() {
